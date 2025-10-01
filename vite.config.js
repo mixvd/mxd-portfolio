70703b40820a6a57dd3228a7c2b7624cad86708f
mixvd <thequentin47@gmail.com>
2025-10-01 03:05:54 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/mxd-portfolio/',
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 });
